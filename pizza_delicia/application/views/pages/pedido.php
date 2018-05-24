bc78ea833fd762f87420f7aad325cd81c8e66a68
Hugo Sousa <hugosousa111@gmail.com>
2018-05-24 01:47:29 -0300

    Responsividade Pedidos

--- pizza_delicia/application/views/pages/pedido.php
+++ pizza_delicia/application/views/pages/pedido.php
@@ -10,39 +10,54 @@
 			</div>
 
 			<form method="post" style="display: none">
-				<label for="nome">Nome:</label>
-				<input type="text" id="nome" name="nome" placeholder="Seu nome">
-				<label for="end">Endereço:</label>
-				<input type="text" name="end" id="end" placeholder="Seu endereço de entrega">
-				<label for="troco">Troco para:</label>
-				<input type="text" name="troco" id="troco" placeholder="ex.: R$ 20,00">
-				<label for="tel">Telefone:</label>
-				<input type="text" name="tel"  id="tel" placeholder="(88) 99999-9999">
-				<label for="pedido">Pedido:</label>
-				<select name="cardapio" id="pedido">
-					<option value="0" selected>Selecione seu Pedido...</option>
-					<option value="1">Mussarela</option>
-					<option value="2">Carne de Sol</option>
-					<option value="3">Bacon</option>
-					<option value="4">Portuguesa</option>
-					<option value="5">4 Queijos</option>
-					<option value="6">Marguerita</option>
-					<option value="7">Cheddar</option>
-					<option value="8">Calabresa</option>
-					<option value="9">Frango c/ Catupiry</option>
-					<option value="10">Mista</option>
-					<option value="11">Camarão</option>
-					<option value="12">Chocolate</option>
-					<option value="13">Brigadeiro</option>
-					<option value="14">Romeu e Julieta</option>
-					<option value="15">Banana com Canela</option>
-					<option value="16">Calzone de Mussarela</option>
-					<option value="17">Calzone de Calabresa</option>
-					<option value="18">Calzone de Carne de Sol</option>
-				</select>
+				<div class="form-row">
+          <div class="col-sm">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" placeholder="Seu nome">
+          </div>
+          <div class="col-sm">
+            <label for="end">Endereço:</label>
+    				<input type="text" name="end" id="end" placeholder="Seu endereço de entrega">
+          </div>
+				</div>
+        <div class="form-row">
+          <div class="col-sm">
+            <label for="troco">Troco para:</label>
+    				<input type="text" name="troco" id="troco" placeholder="ex.: R$ 20,00">
+          </div>
+          <div class="col-sm">
+            <label for="tel">Telefone:</label>
+            <input type="text" name="tel"  id="tel" placeholder="(88) 99999-9999">
+          </div>
+        </div>
+        <div class="form-row">
+          <label for="pedido">Pedido:</label>
+          <select name="cardapio" id="pedido">
+            <option value="0" selected>Selecione seu Pedido...</option>
+            <option value="1">Mussarela</option>
+            <option value="2">Carne de Sol</option>
+            <option value="3">Bacon</option>
+            <option value="4">Portuguesa</option>
+            <option value="5">4 Queijos</option>
+            <option value="6">Marguerita</option>
+            <option value="7">Cheddar</option>
+            <option value="8">Calabresa</option>
+            <option value="9">Frango c/ Catupiry</option>
+            <option value="10">Mista</option>
+            <option value="11">Camarão</option>
+            <option value="12">Chocolate</option>
+            <option value="13">Brigadeiro</option>
+            <option value="14">Romeu e Julieta</option>
+            <option value="15">Banana com Canela</option>
+            <option value="16">Calzone de Mussarela</option>
+            <option value="17">Calzone de Calabresa</option>
+            <option value="18">Calzone de Carne de Sol</option>
+          </select>
+        </div>
+
 				<br><br>
         <div class="row justify-content-center">
           <input type="submit" class="col" name="env" id="enviarpedido" value="Enviar">
-          <input type="reset" class="col" name="limpar" value="Limpar">          
+          <input type="reset" class="col" name="limpar" value="Limpar">
         </div>
 			</form>
