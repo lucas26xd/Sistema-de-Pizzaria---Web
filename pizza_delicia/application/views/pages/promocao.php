22adf11ae2c9cf0c516761a20a8b5a0fc17e78fd
Hugo Sousa <hugosousa111@gmail.com>
2018-05-23 22:34:58 -0300

    Responsividade Promoção

--- pizza_delicia/application/views/pages/promocao.php
+++ pizza_delicia/application/views/pages/promocao.php
@@ -1,14 +1,29 @@
-			<h1 id="promo">#SuperHyperMegaPromo!!!</h1><br>
+<div class="container-fluid">
+	<h1 id="promo" class="img-fluid"	>#Super Hyper Mega Promo!!!</h1><br>
 
-      <img id="gif3" src="<?=base_url('static/imagens/pisca.gif')?>">
+<div class="row">
+	<div class="col-sm-2">
 
-			<br><br>
+	</div>
+	<div class="col-sm-2">
+		<img id="gif3" class="img-fluid" src="<?=base_url('static/imagens/pisca.gif')?>">
+	</div>
+	<div class="col-sm">
+		<br>
+		<h2 id="promo1">Toda TERÇA e QUINTA!</h2>
+	</div>
 
-			<h2 id="promo1">Toda TERÇA e QUINTA!</h2>
+</div>
 
-			<br><br><br>
 
-			<img class="pizzas" src="<?=base_url('static/imagens/calabresa1.png')?>">
-			<img class="pizzas" src="<?=base_url('static/imagens/catupiry1.png')?>">
-      <img class="pizzas" src="<?=base_url('static/imagens/carneseca1.png')?>">
-      
+	<br><br><br>
+	<div class="row">
+  	<div class="col-sm"><img class="img-fluid" src="<?=base_url('static/imagens/calabresa1.png')?>"></div>
+  	<div class="col-sm"><img class="img-fluid"  src="<?=base_url('static/imagens/catupiry1.png')?>"></div>
+  	<div class="col-sm"><img  class="img-fluid"  src="<?=base_url('static/imagens/carneseca1.png')?>"></div>
+	</div>
+
+
+
+
+</div>
