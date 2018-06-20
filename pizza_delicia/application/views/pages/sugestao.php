37abe6bd3a74c53c87ac80394b8bc366b0244043
Hugo Sousa <hugosousa111@gmail.com>
2018-06-20 15:13:08 -0300

    Sugestoes

--- pizza_delicia/application/views/pages/sugestao.php
+++ pizza_delicia/application/views/pages/sugestao.php
@@ -11,17 +11,7 @@
 			<span aria-hidden="true">&times;</span>
 		</button>
 	</div>
-	<div id="errosugestao" class="alert alert-danger alert-dismissible fade show" role="alert" style="display: none;">
-		<h4 class="alert-heading">Erro!</h4>
-		<p>
-			 Você precisa está logado!<br>Por uma questão de organização e autenticidade. Fique tranquilo, seus dados e sugestões são totalmente guardados em sigilo.
-		</p>
-		<hr>
-		<p class="mb-0">Você pode <a class="alert-link" href="<?=site_url('login')?>">Fazer Login</a>! E depois mandar suas sugestões.</p>
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			<span aria-hidden="true">&times;</span>
-		</button>
-	</div>
+
 
 	<br>
 
@@ -33,19 +23,32 @@
 
 	<br><br>
 
-	<form action="./" method="post" style="display: none">
-		<label for="msg">Sua Sugestão:</label>
-		<div class="row">
-			<div class="col-12 col-md-8">
-
-				<textarea name="msg" rows="10" cols="30" id="msg" placeholder="Deixe aqui sua sugestão... =)"></textarea>
-				<input type="submit" id="enviarsugestao" name="b" value="Enviar">
-			</div>
-			<div class="col-6 col-md-3">
-				<img id="gif2" class = "img-fluid" src="<?=base_url('static/imagens/source.gif')?>" >
-				<br>
-			</div>
+	<?php if(!$this->session->has_userdata('id')){ ?>
+		<div class="alert alert-danger alert-dismissible fade show" role="alert">
+			<h4 class="alert-heading">Erro!</h4>
+			<p>
+				 Você precisa está logado!<br>Por uma questão de organização e autenticidade. Fique tranquilo, seus dados e sugestões são totalmente guardados em sigilo.
+			</p>
+			<hr>
+			<p class="mb-0">Você pode <a class="alert-link" href="<?=site_url('login')?>">Fazer Login</a>! E depois mandar suas sugestões.</p>
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
 		</div>
-	</form>
+	<?php } else { ?>
+		<form action="./" method="post" style="display: none">
+			<label for="msg">Sua Sugestão:</label>
+			<div class="row">
+				<div class="col-12 col-md-8">
 
+					<textarea name="msg" rows="10" cols="30" id="msg" placeholder="Deixe aqui sua sugestão... =)"></textarea>
+					<input type="submit" id="enviarsugestao" name="b" value="Enviar">
+				</div>
+				<div class="col-6 col-md-3">
+					<img id="gif2" class = "img-fluid" src="<?=base_url('static/imagens/source.gif')?>" >
+					<br>
+				</div>
+			</div>
+		</form>
+	<?php  } ?>
 </div>
